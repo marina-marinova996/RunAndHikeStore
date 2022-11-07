24903261eca74c36c7c69166c69bd1895b301a1d
marina-marinova996 <marina.marinova996@gmail.com>
2022-11-07 17:57:53 +0200

    Added Shopping Cart View

--- Web/RunAndHikeStore.Web/Controllers/ShoppingCartController.cs
+++ Web/RunAndHikeStore.Web/Controllers/ShoppingCartController.cs
@@ -1,14 +1,28 @@
 ﻿namespace RunAndHikeStore.Web.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
     using RunAndHikeStore.Services.Contracts;
+    using RunAndHikeStore.Web.ViewModels.Product;
+    using RunAndHikeStore.Web.ViewModels.ShoppingCart;
+    using System.Threading.Tasks;
 
     public class ShoppingCartController : BaseController
     {
-        private readonly IShoppingCartService shoppingCartService;
+        //private readonly IShoppingCartService shoppingCartService;
 
-        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        //public ShoppingCartController(IShoppingCartService shoppingCartService)
+        //{
+        //    this.shoppingCartService = shoppingCartService;
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            this.shoppingCartService = shoppingCartService;
+            var model = new ShoppingCartViewModel();
+
+            this.ViewData["Title"] = "Shopping Cart";
+
+            return this.View(model);
         }
     }
 }
